6e3c36649c43ac0c39c1cb0ea6e74c66b1118043
Tauhid Islam <tauhidclover@gmail.com>
2025-08-11 04:31:32 +0600

    First Commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>My Bookstore</h1>
+        <h3>
+            English-USD
+        </h3>
+        <button>Sign In</button>
+    </div>
+  )
+}
+
+export default Navbar
